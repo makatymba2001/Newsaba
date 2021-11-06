7bb7cebdddb34a0f875587a50235f4b9f30f2275
makatymba2001 <makatymba2001@yandex.ru>
2021-11-06 17:33:19 +0200

    Seems good

--- scripts/main/main.js
+++ scripts/main/main.js
@@ -9,17 +9,24 @@ document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('news-max-page').innerText = max_page_count.toString().padStart(2, 0)
 })
 
-function incrementNewsPanel(){
-    news_page = Math.clamp(1, news_page + 1,  max_page_count)
-    selectNewsPage()
-}
+window.addEventListener('resize', function(){
+    var news_item = document.getElementById('news');
+    max_page_count = Math.ceil(news_item.childElementCount / Math.floor(news_item.scrollWidth / news_item.children[0].scrollWidth));
+    document.getElementById('news-max-page').innerText = max_page_count.toString().padStart(2, 0);
+    selectNewsPage();
+})
 
-function decrementNewsPanel(){
-    news_page = Math.clamp(1, news_page - 1, max_page_count)
-    selectNewsPage()
-}
-
-function selectNewsPage(){
+function selectNewsPage(i){
+    news_page = Math.clamp(1, news_page + (i ? i : 0), max_page_count)
     document.getElementById('news').style.top = -100 * (news_page - 1) + '%';
     document.getElementById('news-page').innerText = news_page.toString().padStart(2, 0)
 }
+
+function showMobileMenu(){
+    document.getElementById('mobile-menu').classList.toggle('visible')
+}
+
+document.addEventListener('click', function(e){
+    var mobile_menu = document.getElementById('mobile-menu');
+    if (mobile_menu.classList.contains('visible') && !e.path.includes(mobile_menu) && !e.path.includes(document.getElementById('menu-button'))) mobile_menu.classList.remove('visible');
+})
