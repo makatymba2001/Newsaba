b6190fcdfd9712d9401279621c0e662903c43038
makatymba2001 <makatymba2001@yandex.ru>
2021-11-07 11:08:50 +0200

    Куча всего

--- scripts/main/main.js
+++ scripts/main/main.js
@@ -1,13 +1,14 @@
 Math.clamp = function(min, value, max){
     return Math.max(min, Math.min(max, value));
 }
+
 var news_page, max_page_count;
-document.addEventListener('DOMContentLoaded', function(){
+window.onload = function(){
     news_page = 1;
     var news_item = document.getElementById('news');
     max_page_count = Math.ceil(news_item.childElementCount / Math.floor(news_item.scrollWidth / news_item.children[0].scrollWidth));
     document.getElementById('news-max-page').innerText = max_page_count.toString().padStart(2, 0)
-})
+}
 
 window.addEventListener('resize', function(){
     var news_item = document.getElementById('news');
