43e1b9a7b4682b0be64f7a06db2e9ee572d74eec
makatymba2001 <makatymba2001@yandex.ru>
2021-11-07 12:50:31 +0200

    First page is fully ready

--- scripts/main/main.js
+++ scripts/main/main.js
@@ -31,3 +31,11 @@ document.addEventListener('click', function(e){
     var mobile_menu = document.getElementById('mobile-menu');
     if (mobile_menu.classList.contains('visible') && !e.path.includes(mobile_menu) && !e.path.includes(document.getElementById('menu-button'))) mobile_menu.classList.remove('visible');
 })
+
+function closeLoginMenu(){
+    document.getElementById('login-panel').classList.remove('visible')
+}
+
+function showLoginMenu(){
+    document.getElementById('login-panel').classList.add('visible')
+}
